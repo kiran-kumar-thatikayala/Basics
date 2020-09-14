287ce0aa9b013a4af193a8beee2f489978fcaf1e
Kiran <kt185102@ncr.com>
2020-09-14 10:42:51 +0530

    enhanced performance  bubble sort algo

--- DataStructures/bubblesort.cpp
+++ DataStructures/bubblesort.cpp
@@ -21,9 +21,9 @@ void swap(int *arr, int i, int j)
 
 void bubblesort(int *arr, int count)
 {
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < count-1; i++)
     {
-        for (int j = 0; j < count - i; j++)
+        for (int j = 0; j < count - i-1; j++)
         {   
             if(arr[j]>arr[j+1])
             {
