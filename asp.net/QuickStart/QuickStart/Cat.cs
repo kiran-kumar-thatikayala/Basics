a1676a6b27ee3e696c1d97d00468581594a789b4
Kiran <kt185102@ncr.com>
2020-09-16 21:14:46 +0530

    added new asp.net project with nhibernate

---
+++ asp.net/QuickStart/QuickStart/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuickStart
+{
+    public class Cat
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual char Sex { get; set; }
+
+        public virtual float Weight { get; set; }
+    }
+}
---
+++ asp.net/QuickStart/QuickStart/Controllers/CatController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
+using System.Web.Mvc;
+
+namespace QuickStart.Controllers
+{
+    public class CatController : Controller
+    {
+        // GET: Cat
+        public ActionResult Index()
+        {
+            ISession session = NHibernateHelper.GetCurrentSession();
+            IList<Cat> catList = new List<Cat>();
+            try
+            {
+                using (ITransaction tx = session.BeginTransaction())
+                {
+                    var cats = session.Query<Cat>();
+                    catList = cats.ToList();
+                    tx.Commit();
+                }
+            }
+            finally
+            {
+                NHibernateHelper.CloseSession();
+            }
+            return View(catList);
+        }
+
+        // GET: Cat/Create
+        public ActionResult Create()
+        {
+            ISession session = NHibernateHelper.GetCurrentSession();
+            try
+            {
+                using (ITransaction tx = session.BeginTransaction())
+                {
+                    var princess = new Cat
+                    {
+                        Name = "Princess",
+                        Sex = 'F',
+                        Weight = 7.4f
+                    };
+
+                    session.Save(princess);
+                    tx.Commit();
+                }
+            }
+            catch(Exception exception)
+            {
+                throw exception;
+            }
+            finally
+            {
+                NHibernateHelper.CloseSession();
+            }
+            return View();
+        }
+
+        // POST: Cat/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
